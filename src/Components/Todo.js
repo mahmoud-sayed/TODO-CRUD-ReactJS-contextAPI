651562940b4c2bd8b3025e8c314520ffe1be84ec
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-06-19 20:26:52 +0200

    done working on checkbox

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { FaTrashAlt, FaEdit } from 'react-icons/fa';
 import { DataContext } from '../context/DataContext';
+import axios from 'axios';
 
 const Todo = ({ id, title, completed }) => {
 
-  const { handelDelete, handelEdit } = useContext(DataContext);
+  const DATA_URL = 'http://localhost:100/data';
+  const { handelDelete, handelEdit, items } = useContext(DataContext);
   // handel check
-  const handelCheck = (id) => {
-    console.log(id);
+  const handelCheck = async (id) => {
+    const filteredItem = items.filter(item => item.id === id);
+
+    await axios.put(`${DATA_URL}/${filteredItem[0].id}`, { ...filteredItem[0], completed: !filteredItem[0].completed });
   };
 
 
@@ -20,7 +24,7 @@ const Todo = ({ id, title, completed }) => {
               type="checkbox"
               checked={completed}
               id="check"
-              onClick={() => handelCheck(id)}
+              onChange={() => handelCheck(id)}
             />
             <p>{title}</p>
           </div>
